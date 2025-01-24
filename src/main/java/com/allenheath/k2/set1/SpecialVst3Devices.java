63c924e00d2e944afe4ff3e9010a34d6db75cca9
userdsp <userdsp@gmail.com>
2025-01-24 13:34:35 +0100

    Update SpecialVst3Devices.java for PSP42x 2.0.2

--- src/main/java/com/allenheath/k2/set1/SpecialVst3Devices.java
+++ src/main/java/com/allenheath/k2/set1/SpecialVst3Devices.java
@@ -6,7 +6,7 @@ import com.bitwig.extension.controller.api.DeviceMatcher;
 import com.bitwig.extension.controller.api.SpecificPluginDevice;
 
 public enum SpecialVst3Devices implements SpecialDevice {
-	MEAGAVERB3("5653544D56335867687A206D65676176"), FIX_DOUBLER("048D51804B2049B0BFF8E66CA70F51EC ");
+	    MEAGAVERB3("5653544D56335867687A206D65676176"),FIX_DOUBLER("048D51804B2049B0BFF8E66CA70F51EC"),LEXICON_PSP42("A76DEE9A0ED2481BA85A8FF7D117B741"),; //For the new Lexicon PSP42x 2.0.2
 
 	private final String id;
 
